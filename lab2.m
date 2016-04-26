a39080d17f61509accd2142d8672c3d11cff774a
Piotr Bury <Piotr Bury>
2016-04-26 13:22:47 +0200

    lab2 przerobiony do sprawdzania róznych rozwiązań zadania

--- lab2.m
+++ lab2.m
@@ -14,24 +14,29 @@ A5=mA(sym(pi/2),d5,0,0);
 T03=A1*A2*A3;
 T05=A1*A2*A3*A4*A5
 %%input constants
-d1_input = 1;
-d5_input = 2;
+d1_input = 0.4;
+d5_input = 0.15;
 
 %%input variables
-th1_input = pi/2-pi*rand
-th2_input = pi/4-(pi/2)*rand
-a3_input=0.6-rand*0.4
-th4_input = pi/2-(pi)*rand;
-T05_real=subs(T05,{th1,d1,th2,a3,th4,d5},{th1_input,d1_input, th2_input, a3_input, th4_input, d5_input})
+th1_input = pi/3;
+th2_input = pi/4;
+
+th1_deg = radtodeg(th1_input)
+th2_deg = radtodeg(th2_input)
+
+a3_input= 0.2
+th4_input = pi/3;
+T05_real=subs(T05,{th1,d1,th2,a3,th4,d5},{th1_input,d1_input,pi/4,a3_input,th4_input,d5_input});
 
 %%End effector coordinates from random joint variables
-Px_real = T05_real(1,4)
+Px_real = T05_real(1,4);
 Py_real = T05_real(2,4);
 Pz_real = T05_real(3,4);
-P_real = [Px_real; Py_real; Pz_real];
-Pw = [T05_real(1,3);T05_real(2,3);T05_real(3,3)]*d5_input
-Pa = P_real-Pw
 
-th1_inverse=atan2(Pa(2,1),Pa(1,1))
-th2_inverse=atan2((Pa(3,1)-d1_input),sqrt(Pa(1,1)^2+Pa(2,1)^2))
+P_real = [Px_real; Py_real; Pz_real];
+Pw = [T05_real(1,3);T05_real(2,3);T05_real(3,3)]*d5_input;
+Pa = P_real-Pw;
+
+th1_inverse=radtodeg(atan2(Pa(2,1),Pa(1,1))-pi)
+th2_inverse=radtodeg(atan2(-(Pa(3,1)-d1_input),sqrt(Pa(1,1)^2+Pa(2,1)^2))-pi)
 a3_inverse= sqrt(Pa(1,1)^2+Pa(2,1)^2+(Pa(3,1)-d1_input)^2)
